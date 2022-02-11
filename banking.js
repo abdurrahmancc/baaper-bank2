9e22b8f74f4a2a2fa6b6ef8ea6c9d5192e7cbb33
abdurrahmancc <personalbusinessmail63@gmail.com>
2022-02-11 20:44:26 +0600

    all done

--- banking.js
+++ banking.js
@@ -1,29 +1,65 @@
-const depositTotal = document.getElementById("deposit-total");
-const depositButton =document.querySelector("#deposit-button");
-const depositInput = document.querySelector("#deposit-input");
-const balancetotal = document.querySelector("#balance-total");
-depositButton.addEventListener("click", function(){
-    const totalDeposit = parseFloat(depositTotal.innerText)
-    const inputDeposit = parseFloat(depositInput.value)
-    depositTotal.innerText = totalDeposit + inputDeposit
+//input string to float convert
+function getInputValue(inputId){
+    const inputDeposit = parseFloat(inputId.value);
     depositInput.value="";
-    const totalBalance = parseFloat(balancetotal.innerText);
-    balancetotal.innerText = totalBalance + inputDeposit
+    return inputDeposit ;
+}
+
+//total deposit Amount
+function dipositTotalAmount( totalFieldId, dipositAmount){
+  // debugger;
+  const depositTotal = document.getElementById(totalFieldId);
+  const totalDeposit = parseFloat(depositTotal.innerText);
+  depositTotal.innerText = totalDeposit + dipositAmount;
+}
+
+//total balance
+function updateBalance(dipositAmount, isAdd){
+  const balancetotal = document.getElementById("balance-total");
+  const totalBalance = parseFloat(balancetotal.innerText);
+  if(isAdd==true){
+    balancetotal.innerText = totalBalance + dipositAmount;
+  }
+  else{
+    balancetotal.innerText = totalBalance - dipositAmount;
+  }
+  
+}
+
+function getCurrentBlance(){
+  const balancetotal = document.getElementById("balance-total");
+  const totalBalance = parseFloat(balancetotal.innerText);
+  return totalBalance;
+}
+
+// const depositTotal = document.getElementById("deposit-total");
+const depositButton = document.getElementById("deposit-button");
+const depositInput = document.getElementById("deposit-input");
+depositButton.addEventListener("click", function(){
+  const dipositAmount = getInputValue(depositInput);
+  if(dipositAmount>0){
+    dipositTotalAmount("deposit-total", dipositAmount);
+    updateBalance (dipositAmount, true);
+  }
+  else(
+    alert("Please Enter Your Currect Amount")
+  )
 });
 
 // withdraw balance
-const withdrawTotal = document.querySelector("#withdraw-total");
-const withdrawButton = document.querySelector("#withdraw-button");
-const withdrawInput = document.querySelector("#withdraw-input");
+const withdrawButton = document.getElementById("withdraw-button");
+const withdrawInput = document.getElementById("withdraw-input");
 withdrawButton.addEventListener("click", function(){
-  const inputWithdraw = parseFloat(withdrawInput.value);
-  const totalWithdraw = parseFloat(withdrawTotal.innerText);
-  withdrawTotal.innerText = inputWithdraw + totalWithdraw;
+  const inputWithdraw = getInputValue(withdrawInput)
   withdrawInput.value = "";
-  
-  //update balance
-  balanceUpdate = parseFloat(balancetotal.innerText);
-  balancetotal.innerText = balanceUpdate - inputWithdraw;
-
+//update balance
+  const currentBalance = getCurrentBlance();
+  if(inputWithdraw>0 && inputWithdraw <= currentBalance){
+    dipositTotalAmount ("withdraw-total" , inputWithdraw);
+    updateBalance(inputWithdraw, false);
+  }
+  else(
+    alert("Please Enter Your Currect Amount")
+  )
 })
 
